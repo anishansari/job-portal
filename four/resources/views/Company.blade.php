dbf9490c3f9f75daf02b06c500261eb01e556e32
Anish Ansari <anishforever78@hotmail.com>
2019-02-18 17:54:32 +0530

    Chwnged header of company

--- four/resources/views/Company.blade.php
+++ four/resources/views/Company.blade.php
@@ -6,14 +6,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">WelcomeYou can Post a Job Here</div>
+                <div class="card-header">Welcome You can Post a Job Here</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                
 
                     You may choose an action to get started!!<br><br>
 
